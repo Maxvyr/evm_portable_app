df9cdf96f3d6d69c9e6cd8e25080f619a860a0c3
Maxvyr <maxvid30@gmail.com>
2021-12-08 17:10:41 +0100

    :sparkles: add function to push data and read them to smart contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,7 +4,27 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    //variable contract call state variable and save permanently inside the contract
+    uint256 totalWaves;
+    address senderAddress;
+
     constructor() {
         console.log("Yo yo, My first Contract Smart or Smart Contract");
+    }
+
+    //function public
+    function wave() public {
+        totalWaves+=1;
+        senderAddress = msg.sender;
+        //call variable inside print %s.... , variable
+        //msg.sender c'est l'addresse de la personne qui appel la fonction, le wallet
+        console.log("%s has saved on chain", msg.sender);
+    }
+
+    //function with a return 
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d waves", totalWaves);
+        console.log("And address =>  %d", senderAddress);
+        return totalWaves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,12 +1,34 @@
 //script for compile - deploy - execute
 const main = async () => {
+    //create random user with hardhat
+    //owner => the wallet address of contract owner (person deploy the contract)
+    //randomPerson => a random wallet addres
+    const [owner, randomPerson] = await hre.ethers.getSigners()
     //compile
     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
     //deploy
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
-    //git address SmartContract when it's deploy
+    //print address SmartContract when it's deploy
     console.log("Contract deployed to:", waveContract.address);
+    //print address user owner wallet 
+    console.log("Owner address :", owner.address);
+
+    //Call function
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
+
+    //new user connect
+    //call wave function
+    waveTxn = await waveContract.connect(randomPerson).wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
   };
   
   const runMain = async () => {
